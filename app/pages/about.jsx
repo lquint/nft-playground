8afbcf07b9b7af6fe1846887f0ebbba5769950a6
lquint <83752971+lquint@users.noreply.github.com>
2022-11-30 19:01:50 +0100

    content className added

--- app/pages/about.jsx
+++ app/pages/about.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = function about() {
   return (
-    <div>
+    <div className='content'>
       <h1 className='text-2xl mt-7'>About</h1>
       <p className='mt-5 text-lg'>
         To get more details about this project you can check the github project <a className='underline' href='https://github.com/lquint/nftCenter' target="_blank" rel="noopener noreferrer">here</a>
