93c71fdbf3c3a25a0e46c1872b773b06f3bfab89
lquint <83752971+lquint@users.noreply.github.com>
2022-11-30 16:50:58 +0100

    adapted to real db fields

--- app/pages/transactions.jsx
+++ app/pages/transactions.jsx
@@ -24,7 +24,7 @@ const Transactions = function transactions() {
             <th class="w-1/5">Txn Hash</th>
             <th class="w-1/5">From</th>
             <th class="w-1/5">To</th>
-            <th class="w-1/6">Timestamp</th>
+            <th class="w-1/6">Date (UTC)</th>
           </tr>
         </thead>
         <tbody>
@@ -33,11 +33,11 @@ const Transactions = function transactions() {
         for(let i = 0; i < db.length; i += 1) {
             html += `
             <tr>
-              <td class="text-center text-sm">${db[i].title}</td>
-              <td class="w-1/5 text-center mx-auto"><p class="text-sm mx-auto w-9/12 text-center text-ellipsis overflow-hidden">0x8c6d2b1dd3919ebf44db2762f4efcf3b1d74cbe1699aaf5ab4d25f2e6d12bd74</p></td>
-              <td class="w-1/5 text-center mx-auto"><p class="text-sm mx-auto w-9/12 text-center text-ellipsis overflow-hidden">0xe8f93a2a3b260341bb0bd6d951478cdc56faa703</p></td>
-              <td class="w-1/5 text-center mx-auto"><p class="text-sm mx-auto w-9/12 text-center text-ellipsis overflow-hidden">0x8ba5488f536e379ab35be9f7a4ecb8c41e27baad</p></td>
-              <td class="text-center text-sm">${db[i].content}</td>
+              <td class="text-center text-sm">${db[i].method}</td>
+              <td class="w-1/5 text-center mx-auto"><p class="text-sm mx-auto w-9/12 text-center text-ellipsis overflow-hidden">${db[i].tx}</p></td>
+              <td class="w-1/5 text-center mx-auto"><p class="text-sm mx-auto w-9/12 text-center text-ellipsis overflow-hidden">${db[i].from}</p></td>
+              <td class="w-1/5 text-center mx-auto"><p class="text-sm mx-auto w-9/12 text-center text-ellipsis overflow-hidden">${db[i].to}</p></td>
+              <td class="text-center text-sm">${db[i].timestamp}</td>
             </tr>
             `
 
@@ -56,7 +56,7 @@ const Transactions = function transactions() {
     <div className='mx-auto page-table'>
       <h1 className='text-2xl mt-7'>Transactions</h1>
       <p className='pb-5 mt-5 text-lg'>
-        Transactions made from this web app will be listed right below <a className='underline' href='https://github.com/lquint/nftCenter' target="_blank" rel="noopener noreferrer">here</a>
+        Transactions made from this web app will be listed right below
       </p>
       <div id="displayDB"/>
     </div>
