4f72646778ba4cae3eaf3df13bd03d71a8724b32
lquint <83752971+lquint@users.noreply.github.com>
2022-12-16 23:05:30 +0100

    moved playground to /playground

---
+++ app/pages/playground.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlaygroundTable from '../components/PlaygroundTable';
+
+export default function Home() {
+  return (
+    <div className="content page-standard">
+      <PlaygroundTable />
+    </div>
+  );
+}
