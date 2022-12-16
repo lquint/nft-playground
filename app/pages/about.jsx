63b5ccfc6b0466eb526ebd43c52f59a3f0cacff2
lquint <83752971+lquint@users.noreply.github.com>
2022-12-16 11:22:00 +0100

    text update

--- app/pages/about.jsx
+++ app/pages/about.jsx
@@ -5,7 +5,7 @@ const About = function about() {
     <div className='content'>
       <h1 className='text-2xl mt-7'>About</h1>
       <p className='mt-5 text-lg'>
-        To get more details about this project you can check the github project <a className='underline' href='https://github.com/lquint/nftCenter' target="_blank" rel="noopener noreferrer">here</a>
+        To get more details about this project you can check the github repository <a className='underline' href='https://github.com/lquint/nftCenter' target="_blank" rel="noopener noreferrer">here</a>
       </p>
     </div>
   );
