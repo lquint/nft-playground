649f04a8eeaddaff761c401bdf3ea3f83a0bd9b1
lquint <83752971+lquint@users.noreply.github.com>
2022-12-17 23:27:11 +0100

    modified to keep footer at bottom

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -5,10 +5,10 @@ import Navbar from './navbar';
 
 const Layout = function layout({ children }) {
   return (
-    <div className="">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="h-4/5">{children}</div>
-      <Footer />
+      <div className="grow">{children}</div>
+      <Footer className="mt-auto "/>
     </div>
   );
 };
