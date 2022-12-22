de4585148462323f5cc2f8b1910f7a56794317be
lquint <83752971+lquint@users.noreply.github.com>
2022-12-22 16:01:37 +0100

    fixed some margin and clipboard border

--- app/components/PlaygroundTable.jsx
+++ app/components/PlaygroundTable.jsx
@@ -278,7 +278,7 @@ const PlaygroundTable = function (){
     for (let i = 0; i < tokenIDList.length; i += 1) {
       nftList +=
         `
-            <div class="flex flex-col transition ease-in-out delay-150 border border-black rounded-3xl bg-slate-900 basis-1/4 h-2/5">
+            <div class="flex flex-col transition ease-in-out delay-150 border border-black rounded-3xl bg-[#1D003A] basis-1/4 h-2/5">
                 <div class="relative mx-auto mt-2 overflow-hidden rounded-md w-36 h-36">
                     <Image
                         objectFit='cover'
@@ -368,7 +368,7 @@ const PlaygroundTable = function (){
                 </button>
                 <div class="flex my-auto">
                     <span id="userAddress" class="bg-white px-1 py-1 rounded-full border border-yellow-800 z-10">${userAddress}</span>
-                    <button id="copyClipBoard" class="bg-gray-900 rounded-full z-0 ">
+                    <button id="copyClipBoard" class="bg-gray-900 rounded-full z-0  border border-gray-400">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-plus my-auto mx-1" viewBox="0 0 16 16">
                         <path d="M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3Zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3Z"/>
                         <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5v-1Zm4.5 6V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5a.5.5 0 0 1 1 0Z"/>
@@ -402,7 +402,7 @@ const PlaygroundTable = function (){
 
   return (
     <>
-      <div className="container flex flex-row flex-wrap mx-auto overflow-y-auto bg-sky-400 gap-x-10 gap-y-10 playground">
+      <div className="container flex flex-row flex-wrap mx-auto mb-4 overflow-y-auto border-t border-b gap-x-10 gap-y-10 playground">
         <div
           id="contractBar"
           className="flex flex-row mt-auto ml-4 gap-x-3"
@@ -410,9 +410,8 @@ const PlaygroundTable = function (){
       </div>
       <div
         id="nftDisplay"
-        className="container flex flex-row flex-wrap justify-center h-full mx-auto overflow-y-auto bg-sky-900 gap-x-10 gap-y-10"
+        className="container flex flex-row flex-wrap justify-center h-full mx-auto mb-20 overflow-y-auto gap-x-10 gap-y-10"
       >
-        <Script src="//cdn.jsdelivr.net/npm/sweetalert2@11" />
         <Script
           src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js"
           type="application/javascript"
